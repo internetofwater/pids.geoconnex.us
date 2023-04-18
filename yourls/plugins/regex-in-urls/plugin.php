4f8073f273d41f118c307cf761d990cb7165834e
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2023-04-18 13:50:20 -0400

    Update regex-in-yourls

--- yourls/plugins/regex-in-urls/plugin.php
+++ yourls/plugins/regex-in-urls/plugin.php
@@ -25,15 +25,21 @@ yourls_add_action( 'redirect_keyword_not_found', 'try_regex' );
 function try_regex( $args ) {
     $table         = YOURLS_DB_TABLE_URL;
     $keyword       = $args[0];
-    $sanitized_val = yourls_sanitize_keyword( $keyword );
+    $sanitized_val = '/' . yourls_sanitize_keyword($keyword);
     $pattern       = '/%$';
     $sql           = "SELECT * FROM `$table` WHERE `keyword` LIKE :pattern AND :sanitized_val REGEXP `keyword`";
-    $binds         = array('pattern' => $pattern, 'sanitized_val' => '/'.$sanitized_val);
-    $sql_result    = yourls_get_db()->fetchObject( $sql, $binds );
-    
-    if ($sql_result !== false){
-        for ($i = 0; $sanitized_val[$i] === $sql_result->{"keyword"}[$i+1]; $i++);
-        $redirect_url  = str_replace("$1", substr($sanitized_val, $i), $sql_result->{"url"});
+    $binds         = array('pattern' => $pattern, 'sanitized_val' => $sanitized_val);
+    $sql_result    = yourls_get_db()->fetchObject($sql, $binds);
+
+    if ($sql_result !== false) {
+        $redirect_url = $sql_result->{'url'};
+        $regex        = '/' . preg_replace("/\//", "\\/", $sql_result->{'keyword'}) . '/';
+
+        preg_match($regex, $sanitized_val, $capture_groups);
+        foreach ($capture_groups as $i => $group) {
+            $redirect_url = str_replace('$' . $i, $group, $redirect_url);
+        }
+
         yourls_redirect($redirect_url);
         die();
     }
